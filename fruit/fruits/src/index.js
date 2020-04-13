43d81342a5036afa82deb1821ad22a08ce83a352
neeerajtk <neerajtk560@gmail.com>
2020-04-13 22:15:11 +0530

    index basic

--- fruit/fruits/src/index.js
+++ fruit/fruits/src/index.js
@@ -1,2 +1,6 @@
-import foods from './foods'
-import {choice,remove} from './helpers'
+import foods from './foods';
+import {choice,remove} from './helpers';
+
+let fruit = choice(foods);
+console.log(`I'd like one ${fruit} , please.`);
+console.log(`Here u go ${fruit} .`);
