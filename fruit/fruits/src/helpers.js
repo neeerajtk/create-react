9f739c148c539245a1944b916f27dbd29bce2da9
neeerajtk <neerajtk560@gmail.com>
2020-04-13 22:11:37 +0530

    logic added

--- fruit/fruits/src/helpers.js
+++ fruit/fruits/src/helpers.js
@@ -3,6 +3,11 @@ function choice(items){
 }
 
 function remove(items,item){
+    for(let i=0;i<items.length;i++){
+        if(items[i]===item){
+            return [...items.slice(0,i), ...items.slice(i+1)];
+        }
+    }
 
 }
 
