b6080773b75f234995d0bdcbf83ee3bf5611b95a
neeerajtk <neerajtk560@gmail.com>
2020-04-17 14:24:07 +0530

    rand printing added

--- event/event/src/Update.js
+++ event/event/src/Update.js
@@ -6,17 +6,20 @@ class Update extends Component{
         this.state = {clicked: false};
         this.handleClick = this.handleClick.bind(this);
     }
-    handleClick(e){
+    handleClick(e){ 
+        var rand = 0; 
+        this.rand = Math.floor(Math.random()*10);
         this.setState({clicked:true});
     }
 
+ 
     render(){
         return(
             <div>
                 <h1>
-                    {this.state.clicked ? 'Clicked' : 'Not Clicked'}
+                    {this.state.clicked ? 'Number is '+this.rand : 'Not Clicked'}
                 </h1>
-                <button onClick={this.handleClick}>Click Me</button>
+                <button onClick={this.handleClick}>Random Number</button>
 
             </div>
         )
