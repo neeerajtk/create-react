4083d8bd7102259bf6cd5529455b1c05e158e5ba
neeerajtk <neerajtk560@gmail.com>
2020-04-17 14:45:47 +0530

    state added

--- event/event/src/Update.js
+++ event/event/src/Update.js
@@ -3,13 +3,12 @@ import React, { Component } from 'react'
 class Update extends Component{
     constructor(props){
         super(props);
-        this.state = {clicked: false};
+        this.state = {num: 0};
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick(e){ 
-        var rand = 0; 
-        this.rand = Math.floor(Math.random()*10);
-        this.setState({clicked:true});
+        let rand = Math.floor(Math.random()*10);
+        this.setState({num:rand});
     }
 
  
@@ -17,12 +16,12 @@ class Update extends Component{
         return(
             <div className="wrapper">
                 <h1>
-                    {this.state.clicked ? 'Number is '+this.rand : 'Not Clicked'}
+                    Number is {this.state.num} 
                 </h1>
                 <h2>
-                    {this.rand===7?'Winner':'-'}
+                    {this.state.num===7?'Winner':'-'}
                 </h2>
-                <button className="button" onClick={this.handleClick}>Random Number</button>
+                <button onClick={this.handleClick}>Random Number</button>
 
             </div>
         )
