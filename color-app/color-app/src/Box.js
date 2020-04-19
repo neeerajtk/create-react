4d7c46574673cd3836892d8b32911d57525acc10
neeerajtk <neerajtk560@gmail.com>
2020-04-19 19:49:21 +0530

    base added

---
+++ color-app/color-app/src/Box.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Box extends Component{
+    constructor(props){
+        super(props);
+        this.state = {color: 'purple'};
+    }
+}
+
+export default Box;
