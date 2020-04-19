ef08d593533c971681b82f51095fc367c2d4f5ae
neeerajtk <neerajtk560@gmail.com>
2020-04-19 20:26:17 +0530

    click change color added

--- color-app/color-app/src/Box.js
+++ color-app/color-app/src/Box.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import {choice} from './helpers';
 import './Box.css';
-
 class Box extends Component{
+    static defaultProps = {
+        allColors: ["purple","magenta","red","pink"]
+    };
     constructor(props){
         super(props);
-        this.state = {color: 'purple'};
+        this.state = {color: choice(this.props.allColors)};
         this.handleClick = this.handleClick.bind(this);
     }
+    pickcolor(){
+        let newColor;
+        do {
+            newColor = choice(this.props.allColors);
+        } while(newColor === this.state.color);
+        this.setState({color:newColor});
+    }
     handleClick(){
-
+            this.pickcolor()
     }
     render(){
+        return(
         <div className='Box' style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
 
         </div>
-    }
+        )}
+
 }
 
 export default Box;
