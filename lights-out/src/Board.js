d32d4514d80f38dd1b13a476211abf6760d910c6
neeerajtk <neerajtk560@gmail.com>
2020-04-21 23:30:30 +0530

    handle click added

--- lights-out/src/Board.js
+++ lights-out/src/Board.js
@@ -103,7 +103,8 @@ class Board extends Component {
       let row = [];
         for(let x=0;x<this.props.ncols;x++){
           let coord = `${y}-${x}`
-          row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>)
+          row.push(<Cell key={coord} isLit={this.state.board[y][x]} 
+            flipCellsAroundMe={()=>this.flipCellsAround(coord)} />)
         }
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
