4c6f62597afb2ad05d7b5e8f733a8344191b6747
neeerajtk <neerajtk560@gmail.com>
2020-04-22 00:25:27 +0530

    basic styling added

--- lights-out/src/Board.js
+++ lights-out/src/Board.js
@@ -49,6 +49,7 @@ class Board extends Component {
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
+    
     let board = [];
     // TODO: create array-of-arrays of true/false values
     for(let y=0;y<this.props.nrows;y++){
@@ -89,7 +90,7 @@ class Board extends Component {
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-    let hasWon = false;
+    let hasWon = board.every(row=>row.every(cell=>!cell));
     this.setState({board: board, hasWon: hasWon});
   }
 
@@ -105,6 +106,11 @@ class Board extends Component {
     // make table board
 
     // TODO
+
+    if(this.state.hasWon){
+      return <h1>YOU WON</h1>
+    }
+
     let tblBoard = [];
     for(let y=0;y<this.props.nrows;y++){
       let row = [];
