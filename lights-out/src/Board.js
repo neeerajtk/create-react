f335a5ea7869280ed1469c03e76cf54f9d88762e
neeerajtk <neerajtk560@gmail.com>
2020-04-21 22:24:24 +0530

    board layout added

--- lights-out/src/Board.js
+++ lights-out/src/Board.js
@@ -30,11 +30,20 @@ import './Board.css';
  **/
 
 class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn:0.25
+  };
 
   constructor(props) {
     super(props);
 
     // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,7 +51,15 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
-    return board
+    for(let y=0;y<this.props.nrows;y++){
+      let row = [];
+      for(let x=0; x<this.props.ncols;x++){
+        row.push(Math.random()<this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+
+    }
+    return board;
   }
 
   /** handle changing a cell: update board & determine if winner */
@@ -81,15 +98,19 @@ class Board extends Component {
     // make table board
 
     // TODO
+    let tblBoard = [];
+    for(let y=0;y<this.props.nrows;y++){
+      let row = [];
+        for(let x=0;x<this.props.ncols;x++){
+          row.push(<Cell isLit={this.state.board[y][x]}/>)
+        }
+      tblBoard.push(<tr>{row}</tr>)
+    }
+
     return (
       <table className="Board">
         <tbody>
-          <tr>
-            <Cell isLit={true} />
-            <Cell isLit={false} />
-            <Cell isLit={true} />
-
-          </tr>
+        {tblBoard}
         </tbody>
       </table>
     )
