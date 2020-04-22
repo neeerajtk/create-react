52d6ae4ad1cbbf7ef92a4f837fae9357b1639eb8
neeerajtk <neerajtk560@gmail.com>
2020-04-23 00:13:33 +0530

    boxlist

--- forms/color-box/src/App.js
+++ forms/color-box/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Box from './Box';
+import BoxList from './BoxList';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Box height={10} width={20} color="purple"/>
-      <Box height={15} width={20} color="red"/>
+      <BoxList/>
       
      
     </div>
