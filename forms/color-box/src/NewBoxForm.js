a8c6a91355bf0a72695e2efa3f6bb8580fdb48ff
neeerajtk <neerajtk560@gmail.com>
2020-04-23 12:39:58 +0530

    change handling adde

--- forms/color-box/src/NewBoxForm.js
+++ forms/color-box/src/NewBoxForm.js
@@ -8,6 +8,11 @@ class NewBoxForm extends Component{
             width: "",
             color:""
         };
+        this.handleChange=this.handleChange.bind(this);
+    }
+    handleChange(evt){
+        // 227
+        this.setState({[evt.target.name]: evt.target.value});
     }
    
     render(){
@@ -19,13 +24,23 @@ class NewBoxForm extends Component{
                         type="text" 
                         name="height" 
                         value={this.state.height}
+                        onChange={this.handleChange}
                         id="height"/>
                     <label htmlFor="width">Width</label>
                         <input 
                         type="text"
                         name="width"
                         value={this.state.width}
+                        onChange={this.handleChange}
                         id="width"
+                        />
+                        <label htmlFor="color">Color</label>
+                        <input 
+                        type="text"
+                        name="color"
+                        value={this.state.color}
+                        onChange={this.handleChange}
+                        id="color"
                         />
                 </form>
          
