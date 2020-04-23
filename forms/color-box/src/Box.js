d7d9a45aa58a7a48e01c38ba2a3259f6a85de71d
neeerajtk <neerajtk560@gmail.com>
2020-04-23 15:15:15 +0530

    remove func added

--- forms/color-box/src/Box.js
+++ forms/color-box/src/Box.js
@@ -12,7 +12,7 @@ class Box extends Component{
 
                 }}
                 />
-                <button>X</button>
+                <button onClick={this.props.removeBox}>X</button>
                 
             </div>
         )
