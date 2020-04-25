9b3c12223364175449dd0963241da949224eeec0
neeerajtk <neerajtk560@gmail.com>
2020-04-26 01:21:54 +0530

    ext deck added

--- lifecycle/crd/crds/src/Deck.js
+++ lifecycle/crd/crds/src/Deck.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import axios from "axios";
-const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 class Deck extends Component {
     constructor(props){
         super(props);
-        this.state={
-            deck: null
-        }
+        this.state={deck: null}
+        this.getCard=this.getCard.bind(this);
     }
     async componentDidMount(){
-        let deck= await axios.get(API_URL);
-        this.setState(
-            {
-                deck: deck.data
-            }
+        let deck= await axios.get(`${API_BASE_URL}/new/shuffle/`);
+        this.setState({deck: deck.data}
         );
+    }
+    async getCard(){
+        let id= this.state.deck.deck_id;
+        let cardUrl=`${API_BASE_URL}/${id}/draw`;
+        let cardRes = await axios.get(cardUrl);
     }
 
     render(){
         return (
             <div>
-                <h1>
-                    Card dealer
-                </h1>
+                <h1> Card dealer ! </h1>
+                <button onClick={this.getCard}>Get Card </button>
             </div>
         );
     }
