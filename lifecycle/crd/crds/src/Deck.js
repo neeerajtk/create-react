1786415c09b86df5a16ac525c6a0098ff1ac2027
neeerajtk <neerajtk560@gmail.com>
2020-04-26 01:11:52 +0530

    minor tweaks

--- lifecycle/crd/crds/src/Deck.js
+++ lifecycle/crd/crds/src/Deck.js
@@ -12,7 +12,7 @@ class Deck extends Component {
         let deck= await axios.get(API_URL);
         this.setState(
             {
-                deck: deck.data;
+                deck: deck.data
             }
         );
     }
