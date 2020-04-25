38ebbe6268a786085a7d3f001f4c2a0ea60a0e3d
neeerajtk <neerajtk560@gmail.com>
2020-04-25 18:13:52 +0530

    pro pic and name from git api display added

--- lifecycle/gituserinfo/gituser/src/GithubUserInfo.js
+++ lifecycle/gituserinfo/gituser/src/GithubUserInfo.js
@@ -4,12 +4,16 @@ import axios from "axios";
 class GithubUserInfo extends Component {
     constructor(props){
         super(props);
-        this.state={};
+        this.state={imgUrl:"",name:""};
     }
     async componentDidMount(){
         const url = `https://api.github.com/users/${this.props.username}`;
-        let response = axios.get(url);
-        console.log(url);
+        let response = await axios.get(url);
+        let data=response.data;
+        this.setState({
+            imgUrl:data.avatar_url,
+            name:data.name
+        })
     }
 
     render(){
@@ -17,11 +21,11 @@ class GithubUserInfo extends Component {
             <div>
                 <h1>
                     GITHUB USER INFO
-                   
+        <p><img src={this.state.imgUrl}/><br/> name: {this.state.name}</p>
                     
                 </h1>
             </div>
-    }
+        )}
 }
 
 export default GithubUserInfo;
