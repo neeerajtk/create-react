4ac995ae992dd4a974ad3fcb82459ad0b2ba4745
neeerajtk <neerajtk560@gmail.com>
2020-04-25 17:50:16 +0530

    axios added

--- lifecycle/gituserinfo/gituser/src/GithubUserInfo.js
+++ lifecycle/gituserinfo/gituser/src/GithubUserInfo.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
 class GithubUserInfo extends Component {
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+    async componentDidMount(){
+        const url = `https://api.github.com/users/${this.props.username}`;
+        let response = axios.get(url);
+        console.log(url);
+    }
+
     render(){
         return(
             <div>
                 <h1>
                     GITHUB USER INFO
+                   
+                    
                 </h1>
             </div>
     }
