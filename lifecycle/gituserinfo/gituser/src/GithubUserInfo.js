a776d23b4e97f1cb9df7c207a2867c9b76992620
neeerajtk <neerajtk560@gmail.com>
2020-04-25 17:43:47 +0530

    base files updated

---
+++ lifecycle/gituserinfo/gituser/src/GithubUserInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class GithubUserInfo extends Component {
+    render(){
+        return(
+            <div>
+                <h1>
+                    GITHUB USER INFO
+                </h1>
+            </div>
+    }
+}
+
+export default GithubUserInfo;
