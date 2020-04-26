b870c67640d124e08840368548080d3e26541aa9
neeerajtk <neerajtk560@gmail.com>
2020-04-26 19:42:13 +0530

    multiple jokes pushed to array and disp

--- lifecycle/jokes/jokes/src/JokeList.js
+++ lifecycle/jokes/jokes/src/JokeList.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class JokeList extends Component{
-    async componentDidMount(){
-        let joke = await axios.get("https://icanhazdadjokes.com/");
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    constructor(props){
+        super(props);
+        this.state={jokes: []};
     }
+    async componentDidMount(){
+        let jokes=[];
+        while(jokes.length<this.props.numJokesToGet){       
+            let res = await axios.get("https://icanhazdadjoke.com/",{headers:{Accept:"application/json"}});
+            jokes.push(res.data.joke);
+    }
+    this.setState({jokes: jokes})
+}
     render(){
         return(
-            <div>
+            <div className="JokeList">
                 <h1>Dad Jokes</h1>
+                <div className="JokeList-jokes">
+               {this.state.jokes.map(j=>(
+                    <div>
+                        {j}
+                    </div>
+                ))}
+                </div>
+                
             </div>
         )
-    }
-}
+ }}
+
 export default JokeList;
