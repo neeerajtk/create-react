51533a1e3cfef874b581c198f5b77e860c937ab4
neeerajtk <neerajtk560@gmail.com>
2020-04-26 22:50:48 +0530

    dyn emoji border color added

--- lifecycle/jokes/jokes/src/Joke.js
+++ lifecycle/jokes/jokes/src/Joke.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import './Joke.css';
 
 class Joke extends Component{
+    getColor(){
+        if(this.props.votes >= 15){
+            return "#4CAF50";
+        } else if(this.props.votes>=12){
+            return "#8BC34A";
+        }else if(this.props.votes>=9){
+            return "#CDDC39";
+        }else if(this.props.votes>=6){
+            return "#FFEB3B";
+        }else if(this.props.votes>=3){
+            return "FFC107";
+        }else if(this.props.votes>=0){
+            return "#FF9800";
+        }else{
+            return "#f44336";
+        }
+    }
+
     render(){
         return(
             <div className="Joke">
                 <div className="Joke-buttons">
                     <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-                    <span className="Joke-votes">{this.props.votes}</span>
+                    <span className="Joke-votes">{this.props.votes} style={{borderColor: this.getColor}}</span>
                     <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
                 </div>
                 <div className="Joke-text">
