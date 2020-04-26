0b7e4230579265138e556a2d753a23a41e729ee8
neeerajtk <neerajtk560@gmail.com>
2020-04-26 19:11:42 +0530

    base

---
+++ lifecycle/jokes/jokes/src/JokeList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class JokeList extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Dad Jokes</h1>
+            </div>
+        )
+    }
+}
+export default JokeList;
