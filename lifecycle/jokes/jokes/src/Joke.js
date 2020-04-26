ea5743d497ead4b1cfe8a8c8df9eecf30dd83ea8
neeerajtk <neerajtk560@gmail.com>
2020-04-26 23:06:46 +0530

    dyn emoji added

--- lifecycle/jokes/jokes/src/Joke.js
+++ lifecycle/jokes/jokes/src/Joke.js
@@ -19,6 +19,23 @@ class Joke extends Component{
             return "#f44336";
         }
     }
+    getEmoji(){
+            if(this.props.votes >= 15){
+                return "em em-rolling_on_the_floor_laughing";
+            } else if(this.props.votes>=12){
+                return "em em-laughing";
+            }else if(this.props.votes>=9){
+                return "em em-smiley";
+            }else if(this.props.votes>=6){
+                return "em em-slightly_smiling_face";
+            }else if(this.props.votes>=3){
+                return "em em-neutral_face";
+            }else if(this.props.votes>=0){
+                return "em em-confused";
+            }else{
+                return "em em-angry";
+            }
+        }
 
     render(){
         return(
@@ -32,7 +49,7 @@ class Joke extends Component{
                     {this.props.text}
                 </div>
                 <div className="Joke-smiley">
-                <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+                <i className={this.getEmoji()}></i>
                 </div>
             </div>
         )
