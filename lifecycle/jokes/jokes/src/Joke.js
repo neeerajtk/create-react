b13b6d637911d05a7e7464a124db5caa9754a724
neeerajtk <neerajtk560@gmail.com>
2020-04-26 22:57:09 +0530

    bordercolor issue resolved

--- lifecycle/jokes/jokes/src/Joke.js
+++ lifecycle/jokes/jokes/src/Joke.js
@@ -25,7 +25,7 @@ class Joke extends Component{
             <div className="Joke">
                 <div className="Joke-buttons">
                     <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-                    <span className="Joke-votes">{this.props.votes} style={{borderColor: this.getColor}}</span>
+                    <span className="Joke-votes"  style={{borderColor: this.getColor()}}>{this.props.votes}</span>
                     <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
                 </div>
                 <div className="Joke-text">
