891d0e9e915741083f8e6715f5f30122891179ee
neeerajtk <neerajtk560@gmail.com>
2020-05-11 23:16:50 +0530

    mat ui added

---
+++ context/context-app/src/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Switch from "@material-ui/core/Switch";
+import {withStyles} from "@material-ui/core/styles";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Navbar IS HERE</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
