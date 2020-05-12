d2c31a07434415788bec47846ede810c4dd47d29
neeerajtk <neerajtk560@gmail.com>
2020-05-12 18:56:58 +0530

    form working

--- context/context-app/src/App.js
+++ context/context-app/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider, ThemeContext } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
-function App() {
-  return (
-   <Navbar/>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <LanguageProvider>
+          <PageContent>
+            <Navbar />
+            <Form />
+          </PageContent>
+        </LanguageProvider>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
