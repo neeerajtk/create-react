a169e38e218bf6897de9939da9e9059016cd0137
neeerajtk <neerajtk560@gmail.com>
2020-05-13 00:05:33 +0530

    fully hook

--- context/context-app/src/contexts/LanguageContext.js
+++ context/context-app/src/contexts/LanguageContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const LanguageContext = createContext();
 
 export function LanguageProvider(props) {
-  const [language, setLanguage] = useState("spanish");
+  const [language, setLanguage] = useState("english");
   const changeLanguage = e => setLanguage(e.target.value);
   return (
     <LanguageContext.Provider value={{ language, changeLanguage }}>
