286f162b51993d44847a7f2bd80ce1e450ac2c6d
neeerajtk <neerajtk560@gmail.com>
2020-05-12 23:47:59 +0530

    form to hooks

--- context/context-app/src/contexts/LanguageContext.js
+++ context/context-app/src/contexts/LanguageContext.js
@@ -5,7 +5,7 @@ export const LanguageContext = createContext();
 export class LanguageProvider extends Component {
   constructor(props) {
     super(props);
-    this.state = { language: "spanish" };
+    this.state = { language: "english" };
     this.changeLanguage = this.changeLanguage.bind(this);
   }
   changeLanguage(e) {
