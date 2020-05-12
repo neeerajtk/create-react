db3647459b55a2cba5c36f0754cf9acb69eff41e
neeerajtk <neerajtk560@gmail.com>
2020-05-12 12:05:34 +0530

    base layout

--- context/context-app/src/Navbar.js
+++ context/context-app/src/Navbar.js
@@ -4,14 +4,31 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 import Switch from "@material-ui/core/Switch";
 import {withStyles} from "@material-ui/core/styles";
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Navbar IS HERE</h1>
+            <div className={classes.root}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit">
+                            <span>ABC</span>
+                        </IconButton>
+                        <Typography className={classes.title} variant="h6" color="inherit">
+                            App Title
+                        </Typography>
+                        <Switch/>
+                        <div className={classes.grow}/>
+                        <div className={classes.search}> 
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                        </div>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
