a3fe8d11f92c3b06c3ec7b719c859e227ad980e4
neeerajtk <neerajtk560@gmail.com>
2020-06-08 14:24:34 +0530

    map func

---
+++ jsCncpt/js_concpt.js
@@ -0,0 +1,28 @@
+// Array Concepts 
+
+const myArray = [1,2,3,4,5];
+
+// map();
+
+myArray2 = myArray.map(
+    ele => ele+5
+)
+
+console.log(myArray);
+console.log(myArray2);
+
+
+
+
+
+
+
+
+// filter();
+
+// reduce();
+
+// find();
+
+// includes();
+
