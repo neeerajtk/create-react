411fb9a1f04abbec6a6073fcdb8c8104426b6c65
neeerajtk <neerajtk560@gmail.com>
2020-06-08 14:27:23 +0530

    func def

--- jsCncpt/js_concpt.js
+++ jsCncpt/js_concpt.js
@@ -7,9 +7,14 @@ const myArray = [1,2,3,4,5];
 myArray2 = myArray.map(
     ele => ele+5
 )
+// increments each value of array by 5 
+
+myArray3 = myArray.map(()=>"neeraj");
+// returns arrays with 5 times neeraj element 
 
 console.log(myArray);
 console.log(myArray2);
+console.log(myArray3);
 
 
 
