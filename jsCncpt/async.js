1dbb9f57f9b3faf8a6d17c23435bf793dd1e2f9a
neeerajtk <neerajtk560@gmail.com>
2020-06-09 12:31:55 +0530

    rslve rjct

--- jsCncpt/async.js
+++ jsCncpt/async.js
@@ -1,7 +1,12 @@
 const myPromise = new Promise((resolve, reject)=>{
+    if(false){
     setTimeout(()=>{
         resolve('I have succeeded');
     }, 1000);
+    }else{
+        reject('I have failed !');
+    }
 });
 
-myPromise.then(value => console.log(value));
+myPromise.then(value => console.log(value))
+        .catch(rejectValue => console.log(rejectValue));
