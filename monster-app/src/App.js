5627e5bc84245c1109d68b9d668da21cb4d81f0a
neeerajtk <neerajtk560@gmail.com>
2020-06-10 14:57:19 +0530

    structure updated

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list';
+
 
 class App extends Component{
   constructor(){
@@ -20,6 +22,7 @@ class App extends Component{
   render() {
     return (
     <div className="App">
+      <CardList/>
     {
       
      this.state.monsters.map(monster=>
