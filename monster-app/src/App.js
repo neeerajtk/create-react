1a7b5459c6fa445463a935497fe8694b2fd1c75c
neeerajtk <neerajtk560@gmail.com>
2020-06-11 23:18:52 +0530

    searchbar added

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -23,6 +23,7 @@ class App extends Component{
   render() {
     return (
     <div className="App">
+      <input type="search" placeholder="Search Monster " 
       <CardList monsters={this.state.monsters}/>
     </div>
     )}
