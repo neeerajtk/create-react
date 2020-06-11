2a1b05672268b9a92e83c72cf6649ac6bd7459fa
neeerajtk <neerajtk560@gmail.com>
2020-06-11 22:48:50 +0530

    monster img added

--- monster-app/src/components/card.js
+++ monster-app/src/components/card.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './card.css';
+var a=1;
 const Card = props => (
+
     <div className='card-container'>
+        {/* <img src={`https://robohash.org/${a++}?set=set2`}/> */}
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`}/>
         <h1>{props.monster.name}</h1>
     </div>
 );
