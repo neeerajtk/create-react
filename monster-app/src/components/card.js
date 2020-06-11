68d43cc95c1a8e69287e7f4e386d020c4c3d2e87
neeerajtk <neerajtk560@gmail.com>
2020-06-11 22:58:11 +0530

    img size, email

--- monster-app/src/components/card.js
+++ monster-app/src/components/card.js
@@ -4,9 +4,10 @@ var a=1;
 const Card = props => (
 
     <div className='card-container'>
-        {/* <img src={`https://robohash.org/${a++}?set=set2`}/> */}
+        {/* <img src={`https://robohash.org/${a++}?set=set2&size=180x180`}/> */}
         <img src={`https://robohash.org/${props.monster.id}?set=set2`}/>
-        <h1>{props.monster.name}</h1>
+        <h2>{props.monster.name}</h2>
+        <h3>{props.monster.email}</h3>
     </div>
 );
 
