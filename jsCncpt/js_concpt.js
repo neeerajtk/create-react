6f4fa80d1c5cabaa2da21008fef81b2dc2a570ec
neeerajtk <neerajtk560@gmail.com>
2020-06-13 11:35:20 +0530

    filter

--- jsCncpt/js_concpt.js
+++ jsCncpt/js_concpt.js
@@ -1,29 +1,29 @@
-// Array Concepts 
+// // Array Concepts 
 
-const myArray = [1,2,3,4,5];
+// const myArray = [1,2,3,4,5];
 
-// map();
+// // map();
 
-myArray2 = myArray.map(
-    ele => ele+5
-)
-// increments each value of array by 5 
+// myArray2 = myArray.map(
+//     ele => ele+5
+// )
+// // increments each value of array by 5 
 
-myArray3 = myArray.map(()=>"neeraj");
-// returns arrays with 5 times neeraj element 
+// myArray3 = myArray.map(()=>"neeraj");
+// // returns arrays with 5 times neeraj element 
 
-console.log(myArray);
-console.log(myArray2);
-console.log(myArray3);
-
-
-
-
-
-
-
+// console.log(myArray);
+// console.log(myArray2);
+// console.log(myArray3);
 
 // filter();
+
+const myArray = [1,3,5,7,9];
+
+myArray2 = myArray.filter(el=>el > 4 );
+console.log(myArray2);
+
+
 
 // reduce();
 
