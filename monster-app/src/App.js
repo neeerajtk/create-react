8bf6e1c5b914efea4064d1e2608528bf721c423a
neeerajtk <neerajtk560@gmail.com>
2020-06-15 19:57:30 +0530

    heading

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -34,6 +34,7 @@ class App extends Component{
 
     return (
     <div className="App">
+      <h1>Ghost House !</h1>
      <SearchBox 
         placeholder='Search Monsters '
         handleChange= {this.handleChange}
